db069975d4cd28510cf61892bc0ef29ac229420b
seleb <sean.s.leblanc@gmail.com>
2015-06-27 13:40:19 -0400

    merge

--- O-Jam/Source/O-Jam/src/OJ_Boxer.cpp
+++ O-Jam/Source/O-Jam/src/OJ_Boxer.cpp
@@ -4,7 +4,7 @@
 #include <Box2DSprite.h>
 
 OJ_Boxer::OJ_Boxer(OJ_TexturePack * _texPack, Box2DWorld * _world, int16 _categoryBits, int16 _maskBits, int16 _groupIndex) :
-	OJ_Character(_texPack == nullptr ? new OJ_TexturePack("torso", "hand") : _texPack, _world, _categoryBits, _maskBits, _groupIndex),
+	OJ_Character(_texPack == nullptr ? new OJ_TexturePack("TORSO", "HAND") : _texPack, _world, _categoryBits, _maskBits, _groupIndex),
 	punchSpeed(2.f),
 	punchReach(3.f)
 {
